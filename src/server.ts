0d9858fcf26a837fe2333b76f4f8455f4f4148e7
unknown <YY247@ccs.local>
2025-03-24 20:15:20 +0800

    User Deletion

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import { userRouter } from './routes/users';
-import * as database from './config/database'; 
+import { userRouter } from './routes/user';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,12 +10,6 @@ const PORT = process.env.PORT || 7000;
 app.use(express.json());
 
 app.use('/api', userRouter);
-
-database.connect().then(() => {
-  console.log('Connected to the database');
-}).catch((error: any) => {
-  console.error('Error connecting to the database:', error);
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
